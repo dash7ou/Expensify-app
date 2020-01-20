fc5e0147884ffe4e2f76d6f5cb1539d33260ded2
DasH <fxfddfno@gmail.com>
2020-01-20 21:53:18 +0200

    testing expense list items

---
+++ src/test/components/ExpenseListItems.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { shallow } from "enzyme";
+import ExpenseListItems from "../../components/ExpenseListItems";
+
+// data
+
+const expenses = [
+    {
+        id: "1",
+        description: "dash",
+        amount: 100,
+        createdAt: 0,
+        note: ""
+    }
+]
+
+test("should render ExpenseListItems correctly", ()=>{
+    const wrapper = shallow(<ExpenseListItems expense={expenses[0]} />);
+    expect(wrapper).toMatchSnapshot();
+})
