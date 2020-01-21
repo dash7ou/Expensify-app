d711e9eb7d644873e3c367f13b2d6985557f5b9e
DasH <fxfddfno@gmail.com>
2020-01-21 21:58:17 +0200

    add firebase and setup config

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDMqXtUKeywwXDhFm5JvokwTxAfG6dboQI",
+    authDomain: "expensify-2020.firebaseapp.com",
+    databaseURL: "https://expensify-2020.firebaseio.com",
+    projectId: "expensify-2020",
+    storageBucket: "expensify-2020.appspot.com",
+    messagingSenderId: "568209070857",
+    appId: "1:568209070857:web:69eab74bdb3164050dd1d8",
+    measurementId: "G-FEEZ0Q20F9"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.database().ref().set({
+    name: "mohammed zourob"
+})
