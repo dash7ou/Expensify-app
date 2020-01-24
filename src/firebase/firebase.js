51dbc971f61951a1f267b0a31d9f9f24291561d1
DasH <fxfddfno@gmail.com>
2020-01-24 16:57:52 +0200

    create a separete test database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,14 @@ import * as firebase from "firebase";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDMqXtUKeywwXDhFm5JvokwTxAfG6dboQI",
-    authDomain: "expensify-2020.firebaseapp.com",
-    databaseURL: "https://expensify-2020.firebaseio.com",
-    projectId: "expensify-2020",
-    storageBucket: "expensify-2020.appspot.com",
-    messagingSenderId: "568209070857",
-    appId: "1:568209070857:web:69eab74bdb3164050dd1d8",
-    measurementId: "G-FEEZ0Q20F9"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
