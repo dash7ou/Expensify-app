5c6329b68f4fe8be03365d54d8c06e1dc4e05a0a
DasH <fxfddfno@gmail.com>
2020-01-25 18:46:42 +0200

    add LoginPage and test it

---
+++ src/components/LoginPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const LoginPage = ()=>(
+    <div className="loginpage">
+        <button>Login</button>
+    </div>
+)
+
+export default LoginPage;
--- src/test/components/Header.test.js
+++ src/test/components/Header.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow }from "enzyme";
+import { shallow } from "enzyme";
 import Header from "../../components/Header";
 
 test("should render header correctly", ()=>{
