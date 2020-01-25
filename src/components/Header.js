f7b6b56c8aab6a6589057770ab34008d14cdd349
DasH <fxfddfno@gmail.com>
2020-01-25 19:44:38 +0200

    change in testing header add startLogout function

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import { startLogout, startLogin } from "../actions/auth";
 
 
 
-const Header = ({startLogout})=>(
+export const Header = ({startLogout})=>(
     <header>
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>Home </NavLink>
